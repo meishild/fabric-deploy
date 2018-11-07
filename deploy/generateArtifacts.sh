d254cc4d56ca9c1423f4d82c91a0c2b876844046
haiyang.song <meishild@gmail.com>
2018-11-07 16:57:27 +0800

    edit gitignore

---
+++ deploy/generateArtifacts.sh
@@ -0,0 +1,108 @@
+#!/bin/bash +x
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+#set -e
+
+CHANNEL_NAME=$1
+: ${CHANNEL_NAME:="mychannel"}
+echo $CHANNEL_NAME
+
+export PATH=${PWD}/bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+echo
+
+OS_ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+
+## Using docker-compose template replace private key file names with constants
+function replacePrivateKey () {
+	ARCH=`uname -s | grep Darwin`
+	if [ "$ARCH" == "Darwin" ]; then
+		OPTS="-it"
+	else
+		OPTS="-i"
+	fi
+
+##	cp docker-compose-e2e-template.yaml docker-compose-e2e.yaml
+
+        CURRENT_DIR=$PWD
+        cd crypto-config/peerOrganizations/org1.lychee.com/ca/
+        PRIV_KEY=$(ls *_sk)
+        cd $CURRENT_DIR
+        sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+        cd crypto-config/peerOrganizations/org2.lychee.com/ca/
+        PRIV_KEY=$(ls *_sk)
+        cd $CURRENT_DIR
+        sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+}
+
+## Generates Org certs using cryptogen tool
+function generateCerts (){
+	which cryptogen
+    if [ "$?" -ne 0 ]; then
+        echo "cryptogen tool not found. exiting"
+        exit 1
+    fi
+    echo
+    echo "##########################################################"
+    echo "##### Generate certificates using cryptogen tool #########"
+    echo "##########################################################"
+
+    if [ -d "crypto-config" ]; then
+        rm -Rf crypto-config
+    fi
+    set -x
+    cryptogen generate --config=./crypto-config.yaml
+    res=$?
+    set +x
+    if [ $res -ne 0 ]; then
+        echo "Failed to generate certificates..."
+        exit 1
+    fi
+    echo
+}
+
+## Generate orderer genesis block , channel configuration transaction and anchor peer update transactions
+function generateChannelArtifacts() {
+
+	which configtxgen
+    if [ "$?" -ne 0 ]; then
+        echo "configtxgen tool not found. exiting"
+        exit 1
+    fi
+
+	echo "##########################################################"
+	echo "#########  Generating Orderer Genesis block ##############"
+	echo "##########################################################"
+	# Note: For some unknown reason (at least for now) the block file can't be
+	# named orderer.genesis.block or the orderer will fail to launch!
+	configtxgen -profile TwoOrgsOrdererGenesis2 -outputBlock ./channel-artifacts/genesis.block
+
+	echo
+	echo "#################################################################"
+	echo "### Generating channel configuration transaction 'channel.tx' ###"
+	echo "#################################################################"
+	configtxgen -profile TwoOrgsChannel2 -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+    
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org1MSP   ##########"
+	echo "#################################################################"
+	configtxgen -profile TwoOrgsChannel2 -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+    
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org2MSP   ##########"
+	echo "#################################################################"
+	configtxgen -profile TwoOrgsChannel2 -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+    
+    echo
+}
+
+generateCerts
+replacePrivateKey
+generateChannelArtifacts
